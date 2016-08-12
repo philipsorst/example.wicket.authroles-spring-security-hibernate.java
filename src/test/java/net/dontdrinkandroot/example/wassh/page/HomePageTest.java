be6cd2d86c1bc2a842e0445be065e2b917eeef8c
Philip Washington Sorst <philip@sorst.net>
2016-08-12 19:24:19 +0200

    Adding LogoutLink Test

--- src/test/java/net/dontdrinkandroot/example/wassh/page/HomePageTest.java
+++ src/test/java/net/dontdrinkandroot/example/wassh/page/HomePageTest.java
@@ -1,5 +1,7 @@
 package net.dontdrinkandroot.example.wassh.page;
 
+import org.apache.wicket.util.tester.TagTester;
+import org.junit.Assert;
 import org.junit.Test;
 
 import net.dontdrinkandroot.example.wassh.AbstractWicketTest;
@@ -14,5 +16,19 @@ public class HomePageTest extends AbstractWicketTest
 	{
 		this.tester.startPage(HomePage.class);
 		this.tester.assertRenderedPage(HomePage.class);
+
+		TagTester logoutLinkTester =
+				TagTester.createTagByAttribute(this.tester.getLastResponseAsString(), "wicket:id", "logoutLink");
+		Assert.assertTrue(logoutLinkTester.getAttributeIs("disabled", "disabled"));
+	}
+
+	public void testLogoutLinkEnabledForUser()
+	{
+		this.getSession().signIn("user", "user");
+		this.tester.startPage(HomePage.class);
+
+		TagTester logoutLinkTester =
+				TagTester.createTagByAttribute(this.tester.getLastResponseAsString(), "wicket:id", "logoutLink");
+		Assert.assertFalse(logoutLinkTester.hasAttribute("disabled"));
 	}
 }
